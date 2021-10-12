6efab93bcff170719f8294c92e8a14df589d31ff
Milos <milos.vujcic.997@gmail.com>
2021-10-12 16:28:42 +0200

    dodao sam i za Boston Housing, ucitavanje iz csv fajla i onda pravljenje modela lin reg

--- lin_reg.py
+++ lin_reg.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn import datasets
 
 diabetes = datasets.load_diabetes()
@@ -37,8 +38,41 @@ import seaborn as sns
 #print(Y_test)
 #print(Y_pred)
 
-sns.scatterplot(Y_test, Y_pred, marker = "+")
+#sns.scatterplot(Y_test, Y_pred, marker = "+")
 
-print("asd")
+boston = pd.read_csv("boston.csv")
+
+print(boston)
+
+Y = boston.medv
+print(Y)
+
+X = boston.drop(['medv'], axis = 1)
+print(X)
+
+#ostalo je sve isto
 
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+print(X_train.shape)
+print(Y_train.shape)
+print(X_test.shape)
+print(Y_test.shape)
+
+#definisanje modela
+model = linear_model.LinearRegression()
+#kreiranje modela
+model.fit(X_train, Y_train)
+
+#kreiramo predikcije
+Y_pred = model.predict(X_test)
+
+#ispisujemo neke metrike
+print('Coefficients: ', model.coef_)
+print('Intercept: ', model.intercept_)
+print('Mean squared error(MSE): %.2f' % mean_squared_error(Y_test, Y_pred))
+print('Coeficient of determination (R^2): %.2f' % r2_score(Y_test, Y_pred))
+
+sns.scatterplot(Y_test, Y_pred, alpha = 0.5)
+
+print("pauza")
