291c78c832650f11e38c2f1bfe702bae73a4a4f8
Milos <milos.vujcic.997@gmail.com>
2021-10-12 14:28:36 +0200

    first push

---
+++ lin_reg.py
@@ -0,0 +1,44 @@
+from sklearn import datasets
+
+diabetes = datasets.load_diabetes()
+
+X = diabetes.data
+Y = diabetes.target
+#isto moze i ovako
+X, Y = datasets.load_diabetes(return_X_y=True)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+print(X_train.shape)
+print(Y_train.shape)
+print(X_test.shape)
+print(Y_test.shape)
+
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+#definisanje modela
+model = linear_model.LinearRegression()
+#kreiranje modela
+model.fit(X_train, Y_train)
+
+#kreiramo predikcije
+Y_pred = model.predict(X_test)
+
+#ispisujemo neke metrike
+print('Coefficients: ', model.coef_)
+print('Intercept: ', model.intercept_)
+print('Mean squared error(MSE): %.2f' % mean_squared_error(Y_test, Y_pred))
+print('Coeficient of determination (R^2): %.2f' % r2_score(Y_test, Y_pred))
+
+import seaborn as sns
+
+#print(Y_test)
+#print(Y_pred)
+
+sns.scatterplot(Y_test, Y_pred, marker = "+")
+
+print("asd")
+
+
