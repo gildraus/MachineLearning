f9c55baaa7c9eed2cfe762bf1139d005a54e2f31
milos <milos.vujcic.997@gmail.com>
2021-10-13 13:53:14 +0200

    dodao random forest regressor

--- BasicDataExploration.py
+++ BasicDataExploration.py
@@ -131,3 +131,33 @@ final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=
 
 # fit the final model
 final_model.fit(X, y)
+
+###
+#
+# Random forest regressor
+#
+###
+
+import pandas as pd
+
+
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                      'YearBuilt', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
