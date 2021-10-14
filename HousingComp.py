0dd435856b2f9840252524ee84f52922e58e44d2
milos <milos.vujcic.997@gmail.com>
2021-10-14 17:59:06 +0200

    heatmap bugg

--- HousingComp.py
+++ HousingComp.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
+import seaborn as sb
 
 housing_test_data_path = 'KaggleDatasets/test.csv'
 housing_train_data_path = 'KaggleDatasets/train.csv'
@@ -9,4 +10,11 @@ housing_train_data_path = 'KaggleDatasets/train.csv'
 test = pd.read_csv(housing_test_data_path)
 train = pd.read_csv(housing_train_data_path)
 
-print(test.describe())
+
+#stampamo dimenzije matrice
+print(train.shape)
+#prebrojavamo null vrednosti
+print(train.isnull().sum())
+
+print(sb.heatmap(train.isnull(),yticklabels=False, cbar = False))
+print('2')
