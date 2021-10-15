e89c2c171fa9fa3133f6c8d0d0de7a7c94576e4d
Milos <milos.vujcic.997@gmail.com>
2021-10-15 12:16:21 +0200

    sredjivanje trening seta

--- HousingComp.py
+++ HousingComp.py
@@ -11,10 +11,69 @@ test = pd.read_csv(housing_test_data_path)
 train = pd.read_csv(housing_train_data_path)
 
 
-#stampamo dimenzije matrice
-print(train.shape)
-#prebrojavamo null vrednosti
-print(train.isnull().sum())
 
-sb.heatmap(train.isnull(),yticklabels=False, cbar = False)
-print('s')
+
+col = train.columns
+print(col)
+
+
+
+
+#sredjujemo LotFrontage svaki koji ima null
+mean_lot_frontage = train['LotFrontage'].mean()
+train['LotFrontage'] = train['LotFrontage'].fillna(mean_lot_frontage)
+
+#sredjujemo Alley
+#posto pise da je NA zapravo no alley access ja sam stavio da za svaki NaN
+#vazi da to znaci da zapravo nema alley acces odnosno Naac
+train['Alley'] = train['Alley'].fillna('Naac')
+
+#Masonry veneer type
+train['MasVnrType'] = train['MasVnrType'].fillna('None')
+
+#Masonry veneer area in square feet
+train['MasVnrArea'] = train['MasVnrArea'].fillna(train['MasVnrArea'].mean())
+
+#BsmtQual
+train['BsmtQual'] = train['BsmtQual'].fillna('NoB')
+
+#BsmtCond
+train['BsmtCond'] = train['BsmtCond'].fillna('NoB')
+
+#BsmtExposure
+train['BsmtExposure'] = train['BsmtExposure'].fillna('NoB')
+
+#BsmtFinType1
+train['BsmtFinType1'] = train['BsmtFinType1'].fillna('NoB')
+
+#BsmtFinType2
+train['BsmtFinType2'] = train['BsmtFinType2'].fillna('NoB')
+
+
+
+#prebrojavamo null vrednosti
+#print(train.isnull().sum())
+###
+#
+brojac = 0
+for x in col:
+    flag = train[x].isnull().values.any()
+    if flag == True:
+        print(x)
+        brojac = brojac + 1
+print('Ima ' + str(brojac) + ' kolona sa nan vrednostima ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
