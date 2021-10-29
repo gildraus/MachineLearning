791af9a12db1b7856f2603da7b77b3a2c6965a14
Milos <milos.vujcic.997@gmail.com>
2021-10-29 23:21:16 +0200

    proba submissiona

--- HousingComp.py
+++ HousingComp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import seaborn as sb
 
